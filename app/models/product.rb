522deaf893c953f95946b89d96b9e3d6d7e2876c
CaioBrasetti <caiobrasetti@hotmail.com>
2023-11-21 18:31:49 -0300

    Criação do create view e controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ApplicationRecord
+  CATEGORIES = %w[Console Game Service Accessory Part]
+
   belongs_to :user
 
   validates :name, :price, :description, :quantity, :category, presence: true
   validates :price, presence: true
 
-  validates :category, inclusion: { in: %w[console game service accessory part] }
+  validates :category, inclusion: { in: CATEGORIES }
   validates :description, length: { minimum: 10 }
 end
