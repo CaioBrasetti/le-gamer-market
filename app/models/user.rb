8c8df583dc2c8d24035d4cad0904b8d946616bc1
CaioBrasetti <caiobrasetti@hotmail.com>
2023-11-21 16:05:41 -0300

    Add last_name e first_name na view de users e as validações

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :products
-  # has_many :orders
-  
+  has_many :orders
+  validates :last_name, :first_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
