08a1940ff11c99f4b9175b74dab6912cc91c432c
CaioBrasetti <caiobrasetti@hotmail.com>
2023-11-22 11:57:03 -0300

    Arrumando o seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,14 @@ puts "Database cleaned"
 
 puts "Rodando........."
 user1 = User.create(first_name: "Caio", last_name: "Rocha", email: "caio@teste.com", password: 123123)
-user1.products.create(name: "PS2", price: 4300.50, description: "Ps2 preto em excelente estado", quantity: 2, category: "game")
+user1.products.create(name: "PS2", price: 4300.50, description: "Ps2 preto em excelente estado", quantity: 2, category: "Game")
 
 user2 = User.create(first_name: "Debora", last_name: "lee", email: "debora@teste.com", password: 123123)
-user2.products.create(name: "PS4", price: 4300.50, description: "Ps4 branco em excelente estado", quantity: 5, category: "console")
+user2.products.create(name: "PS4", price: 4300.50, description: "Ps4 branco em excelente estado", quantity: 5, category: "Console")
 
 user3 = User.create(first_name: "Anthony", last_name: "vieira", email: "thony@teste.com", password: 123123)
-user3.products.create(name: "PS5", price: 4300.50, description: "Ps5 branco em excelente estado", quantity: 10, category: "console")
+user3.products.create(name: "PS5", price: 4300.50, description: "Ps5 branco em excelente estado", quantity: 10, category: "Console")
 
 user4 = User.create(first_name: "Pedrão", last_name: "Nascimento", email: "pedro@teste.com", password: 123123)
-user4.products.create(name: "PS1", price: 4300.50, description: "Ps1 branco em excelente estado", quantity: 1, category: "service")
+user4.products.create(name: "PS1", price: 4300.50, description: "Ps1 branco em excelente estado", quantity: 1, category: "Service")
 puts "Concluido"
