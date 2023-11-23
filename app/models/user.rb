249508424e72ed40ce10e0af4f9caa34a708822e
anthony vieira <thony16@gmail.com>
2023-11-23 15:19:11 -0300

    imagens do index implementadas, imagem do perfil do logado implementado, Le Gamer Market na navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :products
   has_many :orders
   validates :last_name, :first_name, presence: true
+  has_one_attached :photo
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
